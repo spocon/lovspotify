0b7076e98da644035e91c8823a55f26985cd4742
Thorsten Haga <spocondeb@gmail.com>
2020-04-27 14:14:19 +0200

    updated version

--- Vagrant/upload.sh
+++ Vagrant/upload.sh
@@ -4,6 +4,6 @@ export UPLOAD=true
 GPG_TTY=$(tty)
 export GPG_TTY
 
-ansible-playbook ../Ansible/start.yml -e lovspotify_version=1.0.2
+ansible-playbook ../Ansible/start.yml -e lovspotify_version=1.0.3
 
 
